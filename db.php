7e175db4bf24547a84f1d993177ba180814a58e3
KhadkaAnkit <ankitkhadka242@gmail.com>
2021-02-18 15:30:17 +0545

    first

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php 
+$host="localhost";
+$user="root";
+$pass="";
+$dbname="krishi sector";
+
+
+$conn=mysql_connect($host,$user,$pass,$dbname) or die();
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Krishi products</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+ <header>
+     <div class="main">
+         
+         <ul>
+             <li class=active><a href=#>Home</a></li>
+             <li><a href=#>Products</a></li>
+             <li><a href=#>About us</a></li>
+             <li><a href=#>Lists</a></li>
+             <li><a href=#>Contacts</a></li>
+         </ul>
+    </div>
+ </header>
+</body>
+</html>
